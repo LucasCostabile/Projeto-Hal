ffa49b9d81beae6c7f8ad6ea64671b1814126c76
LucasCostabile <lucastc95@hotmail.com>
2024-02-12 20:57:21 -0300

    create project

---
+++ entrega/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require('path');
+
+const productsRoutes = require("./routes/productsRoute")
+const cartsRoutes = require("./routes/cartsRoute")
+
+
+const app = express();
+//
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//const staticPath = path.join(`${__dirname}/../public`)
+// verificar na aula o caminho indicado no código
+
+app.use("/static",express.static(staticPath));
+
+
+
+app.use("/products", productsRoutes)
+app.use("/carts", cartsRoutes)
+
+module.exports = app;
