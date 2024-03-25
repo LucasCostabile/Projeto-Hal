2b4b2b1613706f1913b8c9021183e19b48eaeb14
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-03-24 22:38:06 -0300

    corrigido problema de exibição handle

--- entrega/src/routes/products.routes.js
+++ entrega/src/routes/products.routes.js
@@ -14,14 +14,19 @@ const prodRouter = express.Router();
 import { productValidation } from "../middleware/productValidation.js";
 
 prodRouter.get("/", async (req, res) => {
+
   //pegando os valores dos paramentros do browser por query params
   const { limit, page, query, sort } = req.query;
+  console.log(req.query);
+ 
   try {
+    
     //Fazendo a chamada do Service de produtos enviando os valores da query como um objeto para facilitar definir valores por default
     const prods = await getProductsWithPaginate({ limit, page, query, sort });
-    console.log(prods);
+    
+    
     //enviado os valores para a view do objeto prods com a propriedade docs que contem os valores que buscamos no mongoDB.
-    res.render("productsForm", prods.doc);
+    res.render("productsForm", {prods});
   } catch (error) {
     console.log(error);
     res.render("404", { message: "Erro ao listar os produtos!" });
@@ -32,7 +37,7 @@ prodRouter.get("/:pid", async (req, res) => {
   const { pid } = req.params;
   try {
     const productFound = await getProductById(pid);
-    console.log(productFound);
+    
     return res.render("productsForm", { productFound });
   } catch (err) {
     console.log(err);
