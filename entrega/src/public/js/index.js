63af66156fb27cdc11c5237a35f935fc3dc5c137
Henrique Martins <hiikemartins@gmail.com>
2024-03-28 13:17:33 -0300

    alteracao na productForm

--- entrega/src/public/js/index.js
+++ entrega/src/public/js/index.js
@@ -2,44 +2,51 @@ const selectProduct = document.getElementById("select-products");
 const selectPage = document.getElementById("select-page");
 let pageQueryValue;
 
-selectProduct.addEventListener('change', async () => {
-    atualizarLinks(selectProduct.value, 'http://localhost:8080/products');
-    console.log("teste");
-    
+selectProduct.addEventListener("change", async () => {
+  atualizarLinks(selectProduct.value, "http://localhost:8080/products");
+  console.log("teste");
 });
 
-selectPage.addEventListener('click', async(event) => {
-    atualizarLinks(selectProduct.value, 'http://localhost:8080/products');
-
-
+selectPage.addEventListener("click", async (event) => {
+  atualizarLinks(selectProduct.value, "http://localhost:8080/products");
 });
 
-   
+async function handleAddCart() {
+  const productId = document.getElementById("productID").textContent;
 
+  console.log("***(()(*&*()", productId);
 
-function atualizarLinks(limit, baseUrl) {
-    // Seleciona todos os elementos da paginação
-    let elementosPaginacao = document.querySelectorAll('.page-item ');
+  //   const response = await fetch("localhost:8080/cart/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(product),
+  //   });
 
-    // Itera sobre cada elemento
-    for (let i = 0; i < elementosPaginacao.length; i++) {
-        // Obtém o valor da página do atributo 'value'
-        let pagina = elementosPaginacao[i].getAttribute('value');
+  //   if (!response.ok) {
+  //     throw new Error("Erro ao adicionar produto no carrinho");
+  //   }
 
-        // Cria a nova URL
-        let novaUrl = baseUrl + '?limit=' + limit + '&page=' + pagina;
-
-        // Obtém o link dentro do item de paginação
-        let link = elementosPaginacao[i].querySelector('a');
-
-        // Atualiza o atributo 'href' do link
-        link.setAttribute('href', novaUrl);
-        console.log(novaUrl);
-    }
+  //   const cart = await response.json();
+  //   return cart;
 }
 
-// Uso da função
+function atualizarLinks(limit, baseUrl) {
+  // Seleciona todos os elementos da paginação
+  let elementosPaginacao = document.querySelectorAll(".page-item ");
 
+  // Itera sobre cada elemento
+  for (let i = 0; i < elementosPaginacao.length; i++) {
+    // Obtém o valor da página do atributo 'value'
+    let pagina = elementosPaginacao[i].getAttribute("value");
 
+    // Cria a nova URL
+    let novaUrl = baseUrl + "?limit=" + limit + "&page=" + pagina;
 
+    // Obtém o link dentro do item de paginação
+    let link = elementosPaginacao[i].querySelector("a");
 
+    // Atualiza o atributo 'href' do link
+    link.setAttribute("href", novaUrl);
+    console.log(novaUrl);
+  }
+}
