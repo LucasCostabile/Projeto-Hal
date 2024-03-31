5834160b41bcf3b49c0ede61389bc9fc78e80c53
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-03-31 16:56:06 -0300

    criado metodos cart ,pesquisarID, criarNovo carrinho e delete.

--- entrega/src/routes/carts.routes.js
+++ entrega/src/routes/carts.routes.js
@@ -1,25 +1,52 @@
 import express from "express";
-import {createCart}from "../services/cart.service.js";
-import {getProductById,getAllProducts} from "../services/product.service.js";
+import { createCart, deleteCart, getCartById } from "../services/cart.service.js";
+import { getProductById, getAllProducts } from "../services/product.service.js";
 
 const cartRoutes = express.Router();
 
-cartRoutes.get("/:pid",async(req,res)=> {
+cartRoutes.get("/:pid", async (req, res) => {
   const { pid } = req.params;
-  const id='6605e7f788dde60a61239901';
- 
+
   try {
-    const productFound = await getProductById(pid);
-    const cartCreated = await createCart(productFound,id);
-    return cartCreated;
-     
-   // return productFound ;
+    const productFoundCart = await getCartById(pid);
+    return res.render("cart", { productFoundCart });
+
   } catch (err) {
     console.log(err);
     return res.render("404", { message: `Erro ${err}` });
   }
-  
-}) 
 
+})
+
+cartRoutes.post("/", async (req, res) => {
+  // busca idDo carrinho para verificar se ja existe
+ const idCart="6609b83ed74ce852395a21bb" // fazer dinamico 
+
+  const idProduct=req.body.id; 
+   try {
+    const cartID = await getCartById(idCart); 
+  
+    const productFound = await getProductById(idProduct); //  pega product pelo front atravez do id 
+   const cartCreated = await createCart(productFound, cartID);  //envia para service para salvar no banco
+   return cartCreated;
+    
+  } catch (err) {
+    console.log(err);
+    return res.render("404", { message: `Erro ${err}` });
+  }
+
+})
+
+cartRoutes.get("/delete/:cid", async (req, res) => {
+  const { cid } = req.params; // deletar produto tem que digitar o id produto na URL
+  try {
+    const deleteProductCart = await deleteCart(cid);
+    return res.render("cart", { deleteProductCart });
+  } catch (error) {
+    console.log(error);
+    return res.render("404", { message: `Erro ${error}` })
+  }
+
+})
 
 export default cartRoutes;
