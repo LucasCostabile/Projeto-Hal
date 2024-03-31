4dc764862dcd871fd63b0d36042d3869945e17d7
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-03-31 16:54:25 -0300

    modificado evento de add no carrinho

--- entrega/src/public/js/index.js
+++ entrega/src/public/js/index.js
@@ -1,73 +1,91 @@
-const selectProduct=document.getElementById("select-products");
-const selectPage= document.getElementById("select-page");
-const orderPrice=document.getElementById("order-products-price");
-let pageQueryValue=1;
+const selectProduct = document.getElementById("select-products");
+const selectPage = document.getElementById("select-page");
+const orderPrice = document.getElementById("order-products-price");
+let pageQueryValue = 1;
 
 
 if (sessionStorage.getItem('selectProduct')) {
-    selectProduct.value = sessionStorage.getItem('selectProduct');
+   selectProduct.value = sessionStorage.getItem('selectProduct');
 }
 
+
+// limitar produtos por paginas
 selectProduct.addEventListener('change', async () => {
-    sessionStorage.setItem('selectProduct', selectProduct.value);
-    let url=`/products?limit=${selectProduct.value}&page=${pageQueryValue}`;
+   sessionStorage.setItem('selectProduct', selectProduct.value);
+   let url = `/products?limit=${selectProduct.value}&page=${pageQueryValue}`;
    const response = await fetch(url, {
       method: 'get',
       headers: {
          'Content-Type': 'application/json',
       }
    });
-    
+
    if (response.ok) {
-    window.location.href =`/products?limit=${selectProduct.value}`
-        
+      window.location.href = `/products?limit=${selectProduct.value}`
+
 
    } else {
       console.error("Falha ao obter detalhes do produto. Status da resposta:", response.status);
    }
 
-    
+
 });
 
-   selectPage.addEventListener('click', async(event)=>{
-      if(event.target.tagName=="LI"){
-         pageQueryValue = event.target.getAttribute('value');
-         let url=`/products?limit=${selectProduct.value}&page=${pageQueryValue}`;
-         
-      
+//  função selecionar page para produtos
+selectPage.addEventListener('click', async (event) => {
+   if (event.target.tagName == "LI") {
+      pageQueryValue = event.target.getAttribute('value');
+      let url = `/products?limit=${selectProduct.value}&page=${pageQueryValue}`;
+
+
       const response = await fetch(url, {
          method: 'get',
          headers: {
             'Content-Type': 'application/json',
          }
       });
-       
+
       if (response.ok) {
-         window.location.href =`/products?limit=${selectProduct.value}&page=${pageQueryValue}`
-   
+         window.location.href = `/products?limit=${selectProduct.value}&page=${pageQueryValue}`
+
       } else {
          console.error("Falha ao obter detalhes do produto. Status da resposta:", response.status);
       }
-    }
+   }
+});
+// ordenar produtos por preço  asc & desc  / obs: falta ajustar
+orderPrice.addEventListener('change', async () => {
+   sessionStorage.setItem('', selectProduct.value);
+   let url = `/products?limit=${selectProduct.value}&page=${pageQueryValue}&sort=${orderPrice.value}`;
+   const response = await fetch(url, {
+      method: 'get',
+      headers: {
+         'Content-Type': 'application/json',
+      }
    });
 
-   orderPrice.addEventListener('change', async () => {
-      sessionStorage.setItem('', selectProduct.value);
-      let url=`/products?limit=${selectProduct.value}&page=${pageQueryValue}&sort=${orderPrice.value}`;
-     const response = await fetch(url, {
-        method: 'get',
-        headers: {
-           'Content-Type': 'application/json',
-        }
-     });
-      
-     if (response.ok) {
-      window.location.href =`/products?limit=${selectProduct.value}&page=${pageQueryValue}&sort=${orderPrice.value}`
-          
-  
-     } else {
-        console.error("Falha ao obter detalhes do produto. Status da resposta:", response.status);
-     }
-  
-      
-  });
+   if (response.ok) {
+      window.location.href = `/products?limit=${selectProduct.value}&page=${pageQueryValue}&sort=${orderPrice.value}`
+
+
+   } else {
+      console.error("Falha ao obter detalhes do produto. Status da resposta:", response.status);
+   }
+
+
+});
+
+// adiconar produtos ao carrinho buscando pelo id
+const handleAddCart = async (id) => {
+   const requestData = { id: id };
+
+   const response = await fetch("/cart", {
+      method: 'post',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData)
+   })
+
+
+}
