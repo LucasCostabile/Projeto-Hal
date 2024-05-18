f8917dcf7afd0cc38aeca34279c778844e60cdd1
Henrique Martins <hiikemartins@gmail.com>
2024-05-18 15:08:41 -0300

    alteracao na rota de products

--- entrega/src/controllers/product.controller.js
+++ entrega/src/controllers/product.controller.js
@@ -10,27 +10,32 @@ const getAllProducts = async (req, res) => {
   const { limit, page, query, sort } = req.query;
 
   try {
+    console.log("Entrei no GetALL@@@@@@@@@@@");
     //Fazendo a chamada do Service de produtos enviando os valores da query como um objeto para facilitar definir valores por default
     const prods = await getProductsWithPaginate({ limit, page, query, sort });
     let productObjDocs = prods.docs.map((product) => product.toJSON());
 
     //enviado os valores para a view do objeto prods com a propriedade docs e as page.
-    res.render("productsForm", { prods: productObjDocs, page: prods.page });
+    //res.render("productsForm", { prods: productObjDocs, page: prods.page });
+    res.status(200).json(productObjDocs);
   } catch (error) {
     console.log(error);
-    res.render("404", { message: "Erro ao listar os produtos!" });
+    res.status(404).json({ message: "Erro ao listar os produtos!" });
+    //res.render("404", { message: "Erro ao listar os produtos!" });
   }
 };
 
 const getById = async (req, res) => {
   const { pid } = req.params;
   try {
+    console.log("Entrei no getById!!!!!");
     const productFound = await getProductById(pid);
-
-    return res.render("productsForm", { productFound });
+    return res.status(200).json(productFound);
+    //return res.render("productsForm", { productFound });
   } catch (err) {
     console.log(err);
-    return res.render("404", { message: `Erro ${err}` });
+    res.status(404).json({ message: "Erro ao listar os produtos!" });
+    //return res.render("404", { message: `Erro ${err}` });
   }
 };
 
